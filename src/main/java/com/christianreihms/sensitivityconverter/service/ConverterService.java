2a3976bcbe7bec202609a1d681f2099be8540b49
Christian Reihms <65569067+christianreihms@users.noreply.github.com>
2022-06-10 22:16:23 -0500

    Update ConverterService.java
    fixed bug where any DPI deviating from 800 had inverse effects on in-game sensitivity and cm/360 calculation

--- src/main/java/com/christianreihms/sensitivityconverter/service/ConverterService.java
+++ src/main/java/com/christianreihms/sensitivityconverter/service/ConverterService.java
@@ -8,11 +8,11 @@ public class ConverterService {
 
     public float findCm(ConverterModel model) {
         // return cm/360 given sens & dpi
-        return (model.getDpi() / (model.getSens() * model.getMultiplier()));
+        return (800 / (model.getSens() * model.getMultiplier() * (model.getDpi() / 800)));
     }
 
     public float findSens(ConverterModel model) {
         // return in-game sens given cm/360
-        return (model.getDpi() / (model.getCm() * model.getMultiplier()));
+        return (800 / (model.getCm() * model.getMultiplier() * (model.getDpi() / 800)));
     }
 }
